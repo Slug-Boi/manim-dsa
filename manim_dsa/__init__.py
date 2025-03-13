10ecee6e8ba4605853caffa17995a8c40e8e7f6c
Slug-Boi <github.unshipped609@slmail.me>
2025-03-13 10:27:24 +0100

    chore(initpy): add highlightable as an exportable class

--- manim_dsa/__init__.py
+++ manim_dsa/__init__.py
@@ -4,7 +4,16 @@ from .constants import MArrayStyle, MGraphStyle, MStackStyle
 from .m_collection.m_array import MArray
 from .m_collection.m_stack import MStack
 from .m_graph.m_graph import MGraph
+from .utils.utils import Highlightable
 
 __version__ = version(__name__)
 
-__all__ = ["MArray", "MStack", "MGraph", "MArrayStyle", "MStackStyle", "MGraphStyle"]
+__all__ = [
+    "MArray",
+    "MStack",
+    "MGraph",
+    "MArrayStyle",
+    "MStackStyle",
+    "MGraphStyle",
+    "Highlightable",
+]
